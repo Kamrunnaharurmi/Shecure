485e8e886b7002f2208de6efed7ca97add8cd621
Kamrun Nahar <92527223+Kamrunnaharurmi@users.noreply.github.com>
2024-03-03 13:41:01 +0600

    Update rule4.dart

--- lib/rule4.dart
+++ lib/rule4.dart
@@ -73,7 +73,7 @@ class rule4State extends State<rule4> {
                           style: GoogleFonts.salsa(fontSize: 13),
                           "Whoever commits murder shall be punished with death or 1 [imprisonment for life], and shall also be liable to fine.\n(1) Where the death of a woman is caused by any burns or bodily injury or occurs otherwise than under normal circumstances within seven years of her marriage and it is shown that soon before her death she was subjected to cruelty or harassment by her husband or any relative of her husband for, or in connection with, any demand for dowry, such death shall be called “dowry death”, and such husband or relative shall be deemed to have caused her death.Explanation.—For the purposes of this sub-section, “dowry” shall have the same meaning as in section 2 of the Dowry Prohibition Act, 1961 (28 of 1961).\n(2) Whoever commits dowry death shall be punished with imprisonment for a term which shall not be less than seven years but which may extend to imprisonment for life.]\nIf any person commits suicide, whoever abets the commission of such suicide, shall be punished with imprisonment of either description for a term which may extend to ten years, and shall also be liable to fine.\n\n"),
 
-                      Row(
+                      Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           ElevatedButton(
